0d6a2112f336776fe975bc9e2bc42bedecdeb2aa
Jack Sebben <jacksebben11@hotmail.com>
2023-05-19 07:49:07 -0400

    Decided to make the days start at 1 and up as opposed to 0 and up. Looks cleaner and feels 'more' right. Even so, this is just a mini project. This was fun.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,1 +1,12 @@
-puts "hello world"
+
+stock_prices = [17,3,6,9,15,8,6,1,10]
+def stock_picker(stock_prices)
+    buy_sell_days = []
+    buy_sell_days.push(stock_prices.index(stock_prices.min), stock_prices.index(stock_prices.max))
+    profit = stock_prices.max - stock_prices.min
+    puts "\nIf you buy on day #{buy_sell_days[0] + 1} and sell on day #{buy_sell_days[1] + 1},"
+    puts "you will maximize profits at: $#{profit}"
+    puts ""
+end 
+
+stock_picker(stock_prices)
